66ddfeb95ce68e3b1a848ae5723b991d5be99605
BlxccShadow <s3953018@student.rmit.edu.au>
2024-10-10 20:24:30 +1100

    added documentation for terraform code

--- terraform-infra/deployment.tf
+++ terraform-infra/deployment.tf
@@ -1,8 +1,12 @@
+# Variable for Naming the EC2 Instances
+# Change this value to modify the name of the EC2 instances created by this module.
 variable "app_name" {
     type = string
     default = "Foo_App"
     description = "Variable for Naming the EC2 Instances. Change to modify the name output."
 }
+
+# Specifies the required provider for the Terraform configuration.
 terraform {
     required_providers {
         aws = {
@@ -12,10 +16,12 @@ terraform {
     }
 }
 
+# Configure the AWS provider with the region
 provider "aws" {
     region = "us-east-1"
 }
 
+# Retrieves the most recent Ubuntu Server 22.04 HVM AMI
 data "aws_ami" "ubuntu" {
     most_recent = true
     filter {
@@ -29,6 +35,7 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"]
 }
 
+# Creates a VPC with the specified CIDR block and enabled DNS support.
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support = true
@@ -39,12 +46,17 @@ resource "aws_vpc" "main" {
   }
 }
 
+# List of public subnets for the ec2 instances 
 variable "public_subnet_cidrs" {
   type        = list(string)
   description = "Public Subnet CIDR values"
   default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
+#  Creates public subnets within the VPC
+# creates 2 subnets for 2 ec2 instaces
+# Takes in variables from avaliability zones and cidr blocks
+# VPC is defined 
 resource "aws_subnet" "public_subnets" {
   count      = length(var.public_subnet_cidrs)
   vpc_id     = aws_vpc.main.id
@@ -56,13 +68,14 @@ resource "aws_subnet" "public_subnets" {
   }
 }
 
+# list of availability zones for the EC2 instances 
 variable "azs" {
   type        = list(string)
   description = "Availability Zones"
   default     = ["us-east-1a", "us-east-1b"]
 }
 
-
+# Creates an AWS internet gateway resource named "igw"
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 
@@ -71,6 +84,8 @@ resource "aws_internet_gateway" "main" {
   }
 }
 
+# Creates two Amazon EC2 instances for the application deployment.
+# t2 micro tier
 resource "aws_instance" "app" {
   count = 2
   ami = data.aws_ami.ubuntu.id
@@ -84,6 +99,8 @@ resource "aws_instance" "app" {
   }
 }
 
+# Creates a public route table for the VPC
+# Used to route traffic to the right instance 
 resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.main.id
 
@@ -97,6 +114,7 @@ resource "aws_route_table" "public_route_table" {
   }
 }
 
+# Associates public subnets with the public route table.
 resource "aws_route_table_association" "public_subnet" {
   count = length(var.public_subnet_cidrs)
   subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
@@ -104,7 +122,7 @@ resource "aws_route_table_association" "public_subnet" {
 
 }
 
-
+# Creates an Application Load Balancer (ALB) resource in AWS.
 resource "aws_lb" "test" {
   name               = "test-lb-tf"
   internal           = false
@@ -117,6 +135,8 @@ resource "aws_lb" "test" {
   }
 }
 
+# Resource definition for an AWS Application Load Balancer (ALB) target group
+# Health check configuration for the target group
 resource "aws_lb_target_group" "main" {
   name = "target-group-Foo-App"
   port        = 80
@@ -134,6 +154,7 @@ resource "aws_lb_target_group" "main" {
   }
 }
 
+# This resource attaches multiple EC2 instances to an Application Load Balancer (ALB) target group.
 resource "aws_lb_target_group_attachment" "web_server" {
   count = 2
   target_group_arn = aws_lb_target_group.main.arn
@@ -142,6 +163,7 @@ resource "aws_lb_target_group_attachment" "web_server" {
 }
 
 
+# Provision a database server on AWS
 resource "aws_instance" "database" {
     ami = data.aws_ami.ubuntu.id
     instance_type = "t2.micro"
@@ -153,6 +175,7 @@ resource "aws_instance" "database" {
     }
 }
 
+# This resource defines an AWS Key Pair
 resource "aws_key_pair" "admin" {
     key_name = "admin-key-${var.app_name}"
     public_key = file("./local_pub_key.pub")
@@ -202,9 +225,11 @@ resource "aws_security_group" "app_access_config" {
     }
 }
 
+# create security group for the cloud along with LB routing 
 resource "aws_security_group" "db_access_config" {
     name = "db_port_${var.app_name}"
     # SSH
+    # Allow SSH access from anywhere (0.0.0.0/0) on port 22
     ingress {
         from_port = 0
         to_port = 22
@@ -212,6 +237,7 @@ resource "aws_security_group" "db_access_config" {
         cidr_blocks = ["0.0.0.0/0"]
     }
     # PostgreSQL in
+    # Allow inbound traffic for PostgreSQL on port 5432 from anywhere
     ingress {
         from_port   = 0
         to_port     = 5432
@@ -220,6 +246,7 @@ resource "aws_security_group" "db_access_config" {
     }
 
     # HTTP in
+    # Allow inbound HTTP traffic on port 80 from anywhere
     ingress {
         from_port = 0
         to_port = 80
@@ -228,6 +255,7 @@ resource "aws_security_group" "db_access_config" {
     }
 
     # HTTPS out
+    # Allow outbound HTTPS traffic on port 443 to anywhere
     egress {
         from_port = 0
         to_port = 443
@@ -235,6 +263,7 @@ resource "aws_security_group" "db_access_config" {
         cidr_blocks = ["0.0.0.0/0"]
     }
     # PostgreSQL out
+    # Allow outbound PostgreSQL traffic on port 5432 to anywhere
     ingress {
         from_port   = 0
         to_port     = 5432
@@ -244,6 +273,7 @@ resource "aws_security_group" "db_access_config" {
 
 }
 
+#create a bucket which can store the state file for terraform
 terraform {
   backend "s3" {
     # bucket         = "terraform-bucket1234567"  # Abel Bucket
@@ -254,6 +284,7 @@ terraform {
 }
 
 
+# Outputs for Terraform configuration
 output "ini_file" {
     value = "inventory.ini"
 }
